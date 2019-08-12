d279b8b800a2d10a8939ec2e67efb208261866d6
FrencoJobs <frenco.willian@gmail.com>
2019-08-12 20:31:17 +0630

    misc enhancement

--- entranceguide.h
+++ entranceguide.h
@@ -37,87 +37,48 @@ public:
     ~EntranceGuide();
 
 private slots:
-
     void on_male_btn_clicked();
-
     void on_female_btn_clicked();
-
     void on_home_btn_clicked();
-
     void on_guide_btn_clicked();
-
     void on_links_btn_clicked();
-
     void on_abroad_btn_clicked();
-
     void on_about_btn_clicked();
-
     void on_bio_btn_clicked();
-
     void on_eco_btn_clicked();
-
     void on_search_btn_clicked();
-
     void on_search_btn_eco_clicked();
     // links
     void on_pushButton_clicked();
-
     void on_pushButton_2_clicked();
-
     void on_pushButton_3_clicked();
-
     void on_pushButton_4_clicked();
-
     void on_pushButton_5_clicked();
-
     void on_pushButton_6_clicked();
-
     void on_pushButton_7_clicked();
-
     void on_pushButton_8_clicked();
-
     void on_pushButton_9_clicked();
-
     void on_pushButton_10_clicked();
-
     void on_pushButton_11_clicked();
-
     void on_pushButton_12_clicked();
-
     void on_pushButton_13_clicked();
-
     void on_pushButton_14_clicked();
-
     void on_pushButton_15_clicked();
-
     void on_pushButton_16_clicked();
-
     void on_pushButton_17_clicked();
-
     void on_pushButton_18_clicked();
-
     void on_pushButton_19_clicked();
-
     void on_pushButton_20_clicked();
-
     void on_pushButton_21_clicked();
-
     void on_pushButton_22_clicked();
-
     void on_pushButton_23_clicked();
-
     void on_pushButton_24_clicked();
-
     void on_pushButton_25_clicked();
-
     void on_pushButton_26_clicked();
-
     void on_pushButton_27_clicked();
-
     void on_pushButton_28_clicked();
-    // links end
-
     void on_pushButton_29_clicked();
+    // links end
 
 private:
     Ui::EntranceGuide *ui;
